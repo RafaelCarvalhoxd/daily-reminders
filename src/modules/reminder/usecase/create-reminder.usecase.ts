8720e8b5929a0172a838d23407a3d050f0574b90
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 23:54:28 -0300

    feat: add reminder repository inject

--- src/modules/reminder/usecase/create-reminder.usecase.ts
+++ src/modules/reminder/usecase/create-reminder.usecase.ts
@@ -1,9 +1,14 @@
+import { Inject, Injectable } from '@nestjs/common';
 import { Reminder } from 'src/modules/reminder/entity/reminder.entity';
 import { ReminderStatusEnum } from 'src/modules/reminder/enum/reminder-status.enum';
 import { IReminderRepository } from 'src/modules/reminder/interfaces/repository/reminder-repository.interface';
 
+@Injectable()
 export class CreateReminderUseCase {
-  constructor(private readonly reminderRepository: IReminderRepository) {}
+  constructor(
+    @Inject('IReminderRepository')
+    private readonly reminderRepository: IReminderRepository,
+  ) {}
 
   async execute(input: {
     title: string;
