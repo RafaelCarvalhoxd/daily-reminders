0cf3f406bf2004f0a5100a586408850d15e20f8d
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 22:50:39 -0300

    feat: add application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/daily-reminder');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const port = parseInt(process.env.PORT ?? '3000', 10);
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Daily Reminder API')
+    .setDescription('API for Daily Reminder')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, documentBuilder);
+
+  SwaggerModule.setup('docs', app, documentFactory, {
+    useGlobalPrefix: true,
+  });
+
+  await app.listen(port, () => {
+    logger.log(`Server is running on http://localhost:${port}`);
+    logger.log(
+      `Swagger is running on http://localhost:${port}/api/e-commerce/docs`,
+    );
+  });
 }
-bootstrap();
+bootstrap().catch((error) => {
+  const logger = new Logger('Bootstrap');
+  logger.error('Erro ao iniciar a aplicação', error);
+  process.exit(1);
+});
