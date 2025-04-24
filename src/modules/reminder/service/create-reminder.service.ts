88ea13fa160d72bab963e86da1e09760feb38963
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 22:21:52 -0300

    feat: add create reminder service

---
+++ src/modules/reminder/service/create-reminder.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Reminder } from 'src/modules/reminder/entity/reminder.entity';
+import { ReminderStatusEnum } from 'src/modules/reminder/enum/reminder-status.enum';
+import { ICreateReminderService } from 'src/modules/reminder/interfaces/service/create-reminder.service';
+import { CreateReminderUseCase } from 'src/modules/reminder/usecase/create-reminder.usecase';
+
+@Injectable()
+export class CreateReminderService implements ICreateReminderService {
+  constructor(private readonly createReminderUseCase: CreateReminderUseCase) {}
+
+  async execute(input: {
+    title: string;
+    description: string;
+    status: ReminderStatusEnum;
+    dueDates: Date[];
+  }): Promise<Reminder> {
+    return await this.createReminderUseCase.execute(input);
+  }
+}
