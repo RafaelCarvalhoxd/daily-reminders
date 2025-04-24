d4e12c690c09aad57bd9d7a012bb2b47a1403075
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 22:22:40 -0300

    feat: add reminder dto response

---
+++ src/modules/reminder/dto/response/reminder-response.dto.ts
@@ -0,0 +1,29 @@
+export class ReminderResponseDto {
+  public readonly id: string;
+  public readonly title: string;
+  public readonly description: string;
+  public readonly status: string;
+  public readonly dueDates: {
+    date: Date;
+  }[];
+  public readonly createdAt: Date;
+  public readonly updatedAt: Date;
+
+  constructor(
+    id: string,
+    title: string,
+    description: string,
+    status: string,
+    dueDates: Date[],
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+    this.dueDates = dueDates.map((date) => ({ date }));
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
