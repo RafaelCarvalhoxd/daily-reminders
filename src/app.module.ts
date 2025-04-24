ef57b53fba613300bc669e72dcbfccd8c7457753
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 22:52:03 -0300

    feat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InfraModule } from 'src/infra/infra.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [InfraModule],
 })
 export class AppModule {}
