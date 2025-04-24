500efc99dfcd53218e555265ac1faf8c91d7f6a2
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 23:53:05 -0300

    feat: add create reminder controller

---
+++ src/modules/reminder/controller/create-reminder.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateReminderRequestDto } from 'src/modules/reminder/dto/request/create-reminder-request.dto';
+import { ReminderResponseDto } from 'src/modules/reminder/dto/response/reminder-response.dto';
+import { ICreateReminderService } from 'src/modules/reminder/interfaces/service/create-reminder.service';
+
+@ApiTags('Reminder')
+@Controller('Reminder')
+export class CreateReminderController {
+  constructor(
+    @Inject('ICreateReminderService')
+    private readonly service: ICreateReminderService,
+  ) {}
+
+  @Post()
+  async createReminder(
+    @Body() dto: CreateReminderRequestDto,
+  ): Promise<ReminderResponseDto> {
+    const reminder = await this.service.execute({
+      title: dto.title,
+      description: dto.description,
+      status: dto.status,
+      dueDates: dto.dueDates,
+    });
+
+    return new ReminderResponseDto(
+      reminder.getId(),
+      reminder.getTitle(),
+      reminder.getDescription(),
+      reminder.getStatus(),
+      reminder.getDueDates().map((dueDate) => dueDate.getDate()),
+      reminder.getCreatedAt(),
+      reminder.getUpdatedAt(),
+    );
+  }
+}
