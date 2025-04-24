8887d5bc5647366b99f34e8d87d566981f2aac88
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-23 23:54:48 -0300

    feat: add reminder module

---
+++ src/modules/reminder/reminder.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateReminderController } from 'src/modules/reminder/controller/create-reminder.controller';
+import { DueDateModel } from 'src/modules/reminder/repository/models/due-date.entity';
+import { ReminderModel } from 'src/modules/reminder/repository/models/reminder.entity';
+import { ReminderRepository } from 'src/modules/reminder/repository/reminder.repository';
+import { CreateReminderService } from 'src/modules/reminder/service/create-reminder.service';
+import { CreateReminderUseCase } from 'src/modules/reminder/usecase/create-reminder.usecase';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ReminderModel, DueDateModel])],
+  controllers: [CreateReminderController],
+  providers: [
+    CreateReminderUseCase,
+    {
+      provide: 'ICreateReminderService',
+      useClass: CreateReminderService,
+    },
+    {
+      provide: 'IReminderRepository',
+      useClass: ReminderRepository,
+    },
+  ],
+})
+export class ReminderModule {}
