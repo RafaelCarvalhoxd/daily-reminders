d84895fb062410b889d327fc57312e76e125a54f
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-29 23:34:37 -0300

    feat: add shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InfraModule } from 'src/infra/infra.module';
 import { ModulesModule } from 'src/modules/modules.module';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
-  imports: [InfraModule, ModulesModule],
+  imports: [InfraModule, ModulesModule, SharedModule],
 })
 export class AppModule {}
