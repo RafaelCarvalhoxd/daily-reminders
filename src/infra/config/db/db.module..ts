33353d17f8519a05a2915cda6eb200892c35c76c
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-30 14:14:20 -0300

    feat: add user model

--- src/infra/config/db/db.module..ts
+++ src/infra/config/db/db.module..ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { ReminderModel } from 'src/modules/reminder/repository/models/reminder.entity';
 import { DueDateModel } from 'src/modules/reminder/repository/models/due-date.entity';
+import { UserModel } from 'src/modules/user/repository/models/user.model';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { DueDateModel } from 'src/modules/reminder/repository/models/due-date.en
         username: configService.get<string>('database.username'),
         password: configService.get<string>('database.password'),
         database: configService.get<string>('database.database'),
-        entities: [ReminderModel, DueDateModel],
+        entities: [ReminderModel, DueDateModel, UserModel],
         autoLoadEntities: true,
         synchronize: true,
         logging: true,
