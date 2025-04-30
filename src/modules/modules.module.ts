4408e9315cf1042bda222faae47b78e423f09969
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-04-30 14:14:53 -0300

    feat: add auth module

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/modules/auth/auth.module';
 import { ReminderModule } from 'src/modules/reminder/reminder.module';
 import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
-  imports: [ReminderModule, UserModule],
-  exports: [ReminderModule, UserModule],
+  imports: [ReminderModule, UserModule, AuthModule],
+  exports: [ReminderModule, UserModule, AuthModule],
 })
 export class ModulesModule {}
