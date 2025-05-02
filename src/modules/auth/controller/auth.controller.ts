b1e4d8a00392e29db0543a9ef2e5566535183a1b
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-05-02 16:37:52 -0300

    feat: add login user auth endpoint

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { LoginUserRequestDto } from 'src/modules/auth/dto/request/login-user-auth-request.dto';
 import { RegisterUserRequestDto } from 'src/modules/auth/dto/request/register-user-auth-request.dto';
+import { ILoginUserAuthService } from 'src/modules/auth/interface/service/login-user-auth.service';
 import { IRegisterUserAuthService } from 'src/modules/auth/interface/service/register-user-auth.service';
 import { UserResponseDto } from 'src/modules/user/dto/response/user-response.dto';
 
@@ -10,6 +12,8 @@ export class AuthController {
   constructor(
     @Inject('IRegisterUserAuthService')
     private readonly registerUserService: IRegisterUserAuthService,
+    @Inject('ILoginUserAuthService')
+    private readonly loginUserService: ILoginUserAuthService,
   ) {}
 
   @Post('/register')
@@ -25,5 +29,15 @@ export class AuthController {
     });
 
     return UserResponseDto.toJson(user);
+  }
+
+  @Post('/login')
+  async loginUser(
+    @Body() dto: LoginUserRequestDto,
+  ): Promise<{ token: string }> {
+    return await this.loginUserService.execute({
+      email: dto.email,
+      password: dto.password,
+    });
   }
 }
