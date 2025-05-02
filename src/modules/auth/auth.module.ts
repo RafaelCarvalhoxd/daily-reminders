aeeae24c02368a3ae78c3dea86203db682aa84cd
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-05-02 16:38:20 -0300

    feat: add login user auth provider

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from 'src/modules/auth/controller/auth.controller';
+import { LoginUserAuthService } from 'src/modules/auth/service/login-user-auth.service';
 import { RegisterUserAuthService } from 'src/modules/auth/service/register-user-auth.service';
+import { LoginUserAuthUseCase } from 'src/modules/auth/usecase/login-user-auth.usecase';
 import { RegisterUserAuthUseCase } from 'src/modules/auth/usecase/register-user-auth.usecase';
 import { UserModule } from 'src/modules/user/user.module';
 
@@ -9,6 +11,11 @@ const providers = [
   {
     provide: 'IRegisterUserAuthService',
     useClass: RegisterUserAuthService,
+  },
+  LoginUserAuthUseCase,
+  {
+    provide: 'ILoginUserAuthService',
+    useClass: LoginUserAuthService,
   },
 ];
 
