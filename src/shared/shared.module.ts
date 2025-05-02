74aadaf7350b14e6940fdf5a7af8cd4f6192228d
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-05-02 17:22:25 -0300

    feat: add guard module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { GuardModule } from 'src/shared/guard/guard.module';
 import { LibModule } from 'src/shared/libs/lib.module';
 
 @Module({
-  imports: [LibModule],
+  imports: [LibModule, GuardModule],
 })
 export class SharedModule {}
