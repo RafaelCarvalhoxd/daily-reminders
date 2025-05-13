bdf2496c9ed38fe8721a85dbf55e0ba34f8fcc13
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2025-05-13 14:49:19 -0300

    feat: add bull & event emitter module

--- src/shared/libs/lib.module.ts
+++ src/shared/libs/lib.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BcryptModule } from 'src/shared/libs/bcrypt/bcrypt.module';
+import { BullModule } from 'src/shared/libs/bull/bull.module';
+import { EventEmitterServiceModule } from 'src/shared/libs/event-emitter/event.emitter.module';
 import { JwtServiceModule } from 'src/shared/libs/jwt/jwt.module';
 import { MailModule } from 'src/shared/libs/mail/mail.module';
 
 @Module({
-  imports: [BcryptModule, JwtServiceModule, MailModule],
+  imports: [
+    BcryptModule,
+    JwtServiceModule,
+    MailModule,
+    BullModule,
+    EventEmitterServiceModule,
+  ],
 })
 export class LibModule {}
